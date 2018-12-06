fa75c82e527e04bc9e1e0d4d2ff25bf05c940884
Frances Thai <notfelineit@gmail.com>
2018-12-05 23:26:06 -0800

    Add initial exquisite corpses component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react"
+import ExquisiteCorpse from './ExquisiteCorpse'
 
 import '../styles/App.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>My React App!</h1>
-      </div>
+      <ExquisiteCorpse />
     )
   }
 }
---
+++ src/components/ExquisiteCorpse.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const exquisiteCorpseStyle = {
+  backgroundColor: 'black',
+  height: '100vh',
+  width: '100vw',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  color: 'white'
+}
+const ExquisiteCorpse = () => {
+  return <div style={exquisiteCorpseStyle}>Hello!!!</div>
+}
+export default ExquisiteCorpse
